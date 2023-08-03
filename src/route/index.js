0991a1eba0be83c16c59c4275bd9b871adceb27d
davyd1matv <davydmatviienko@gmail.com>
2023-08-03 13:58:34 +0300

    slack lesson

--- src/route/index.js
+++ src/route/index.js
@@ -1222,7 +1222,9 @@ router.get('/slack', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('slack', {})
+  res.render('slack', {
+    // layout: 'slack',
+  })
 })
 
 // ================================================================
